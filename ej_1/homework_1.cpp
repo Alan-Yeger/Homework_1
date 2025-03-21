4802001188970f5aa87ff2f6dcd517fb2bf67a56
root <root@Notebook-Alan>
2025-03-21 11:17:01 -0300

    Added comments to ex1

--- ej_1/homework_1.cpp
+++ ej_1/homework_1.cpp
@@ -4,19 +4,28 @@
 
 using namespace std;
 
-void mtx_nxn(vector<vector<int>>& mtx, int n);
+/*
+En este ejercicio hago uso del contenedor vector, ya que estoy acostumbrado a usarlo en los problemas de competitiva.
+Lo único que se hace en este archivo es recibir un entero n por consola, armar un contenedor de nxn y llamar a las 
+funciones para armar la matriz e imprimirla de mayor a menor.
+*/
+
+void mtx_nxn(vector<vector<int>>& mtx, int n); //Prototipos de funciones que reciben la referencia de la matriz y el entero n.
 void mayor_menor(vector<vector<int>>& mtx, int n);
 
 int main(){
 
     int n = 0;
 
+    // Se recibe el número solo cuando sea mayor que 1.
     while(n <= 1) {
         cout << "Ingrese un entero positivo mayor que 1 \n";
         cin >> n;
     }
+    //Defino un vector de vectores de nxn como mi contenedor para la matriz.
     vector<vector<int>> mtx(n, vector<int>(n));
     
+    //Invocación de las funciones
     mtx_nxn(mtx, n);
     mayor_menor(mtx, n);
 }
--- ej_1/homework_1.hpp
+++ ej_1/homework_1.hpp
@@ -5,15 +5,16 @@ using namespace std;
 
 void mtx_nxn(vector<vector<int>>& mtx, int n) {
     int v = 1;
+    //Recorremos para cada fila las n columnas y vamos asignando los valores de cada casilla en orden creciente.
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             mtx[i][j] = v;
             v++;
         }
     }
-
+    //Imprimimos el resultado de la matriz ordenada
     for (int i = 0; i < n; ++i) {
-        for (int v: mtx[i]) {
+        for (const int v: mtx[i]) {
             cout << v << " ";
         }
         cout << "\n";
@@ -26,7 +27,10 @@ void mtx_nxn(vector<vector<int>>& mtx, int n) {
 
 void mayor_menor(vector<vector<int>>& mtx, int n) {
     int f, c;
-
+    //Usamos el típico truquito para recorrer una matriz con un solo for (aunque el orden sigue siendo O(n^2)) dividiendo
+    //la coordenada en en su fila y su columna. Notemos que si numeramos las casillas en orden creciente desde 0 hasta n*n-1
+    //el número de fila es nro_casilla/n y el número de columna es nro_casilla%n. Para recorrerla en orden decreciente empezamos
+    //en la casilla n*n-1 y terminamos en la 0.
     for (int i = n * n - 1; i >= 0; --i) {
         f = i / n;
         c = i % n;
